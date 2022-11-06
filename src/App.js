de3d4f48039d05cdca2545da6371e104fbb1905f
backlitRO <cvlad1508@gmail.com>
2022-11-07 01:24:42 +0200

    first try implement login

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
+import React, { createContext, useReducer } from 'react';
 import Router from './routes';
 import ThemeProvider from './theme';
 import ScrollToTop from './components/ScrollToTop';
+import { initialState, reducer } from "./store/reducer";
+
+export const AuthContext = createContext();
 
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
   return (
-    <ThemeProvider>
+    <AuthContext.Provider
+      value={{
+        state,
+        dispatch
+      }}
+    >
+      <ThemeProvider>
         <ScrollToTop />
         <Router />
-    </ThemeProvider>
+      </ThemeProvider>
+    </AuthContext.Provider>
   );
 }
 
--- src/layouts/dashboard/header/index.js
+++ src/layouts/dashboard/header/index.js
@@ -1,10 +1,11 @@
+import React, { useContext, useState, useEffect, } from "react";
+import { AuthContext } from "../../../App";
+
 import { alpha, styled } from '@mui/material/styles';
 import {
   AppBar,
   Toolbar,
   Typography,
-  Container,
-  Grid,
   IconButton,
 } from '@mui/material';
 import { useTheme } from '@mui/material/styles';
@@ -12,8 +13,9 @@ import { useTheme } from '@mui/material/styles';
 import { createStyles, makeStyles } from "@material-ui/core/styles";
 import logo from "../../../logo.svg";
 import exit from "../../../assets/exit.svg";
+import account from "../../../assets/account.svg";
 
-import Login from "../../../components/Login"
+// import Login from "../../../components/Login"
 
 const BG_COLOR = '#ffffff';
 
@@ -61,15 +63,102 @@ const useStyles = makeStyles((theme) =>
 
 
 export default function DashboardNavbar() {
+  const { state, dispatch } = useContext(AuthContext);
+
   const classes = useStyles();
+
+  const accountInfo = () => {
+    const { avatar_url, name, public_repos, followers, following } = state.user;
+    return (
+      <div style={{ marginLeft: 'auto' }}>
+        <img src={avatar_url} alt="Avatar" style={{ height: '2em' }} />
+        <div>{name}</div>
+      </div>
+    )
+  }
+
+  const [data, setData] = useState({ errorMessage: "", isLoading: false });
+
+  const { client_id, redirect_uri } = state;
+
+  useEffect(() => {
+    console.log(state.isLoggedIn);
+    if (!state.isLoggedIn) {
+      // After requesting Github access, Github redirects back to your app with a code parameter
+      const url = window.location.href;
+      const hasCode = url.includes("?code=");
+
+      // If Github API returns the code parameter
+      if (hasCode) {
+        const newUrl = url.split("?code=");
+        window.history.pushState({}, null, newUrl[0]);
+        setData({ ...data, isLoading: true });
+
+        const requestData = {
+          code: newUrl[1]
+        };
+
+        const proxy_url = state.proxy_url;
+
+        // Use code parameter and other parameters to make POST request to proxy_server
+        fetch(proxy_url, {
+          method: "POST",
+          body: JSON.stringify(requestData)
+        })
+          .then(response => response.json())
+          .then(data => {
+            dispatch({
+              type: "LOGIN",
+              payload: { user: data, isLoggedIn: true }
+            });
+          })
+          .catch(error => {
+            setData({
+              isLoading: false,
+              errorMessage: "Sorry! Login failed"
+            });
+          });
+      }
+    }
+  }, [state, dispatch, data]);
+
+
+
+
+
+  const handleLogout = () => {
+    dispatch({
+      type: "LOGOUT"
+    });
+  }
 
   return (
     <AppBar sx={{ boxShadow: 0, bgcolor: BG_COLOR }}>
       <ToolbarStyle disableGutters>
         <img src={logo} alt="" className={classes.logo} />
         <TextTypography>FilPulse</TextTypography>
-        <Login />
-        <IconButton style={{ marginRight: '6.75em' }}>
+        {state.isLoggedIn ? accountInfo() : ''}
+        {data.isLoading ? (
+          <div>
+            loading
+          </div>
+        ) : (
+          <div style={{ marginLeft: 'auto' }}>
+            {
+              // Link to request GitHub access
+            }
+            <a
+              className="login-link"
+              href={`https://github.com/login/oauth/authorize?scope=user&client_id=${client_id}&redirect_uri=${redirect_uri}`}
+              onClick={() => {
+                setData({ ...data, errorMessage: "" });
+              }}
+            >
+              <img src={account} alt="account" />
+            </a>
+          </div>
+        )}
+        <IconButton style={{ marginRight: '6.75em' }} onClick={() => handleLogout()}>
           <img src={exit} alt="exit" />
         </IconButton >
       </ToolbarStyle>
