f425f994451e709c6a7c6c830a5e35435ad33c3b
yellow-cube <93196588+yellow-cube@users.noreply.github.com>
2022-11-11 19:33:37 +0200

    Update WatchlistHead.js

--- src/components/watchlistTable/WatchlistHead.js
+++ src/components/watchlistTable/WatchlistHead.js
@@ -128,7 +128,7 @@ export default function WatchlistHead({
                             Participants
                         </Typography>
 
-                        <TriunghiMenuWLParticipants data={data} />
+                       
 
                         <IconButton
                             id="basic-button"
