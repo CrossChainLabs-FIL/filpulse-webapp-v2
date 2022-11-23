cb1968b97277085112cba0eedb11b0b921cc6081
yellow-cube <93196588+yellow-cube@users.noreply.github.com>
2022-11-23 16:46:53 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 
-//export const API = 'http://34.70.48.69:8083/';
-export const API = 'http://34.134.184.255/';
-// export const API = 'http://localhost:5000/';
+export const API = 'https://api.filpulse.io/';
+//export const API = 'http://34.134.184.255/';
+//export const API = 'http://localhost:5000/';
 export const PADDING = 92;
